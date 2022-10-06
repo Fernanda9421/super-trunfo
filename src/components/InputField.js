91d986b0cd71556ab8676afb8cdc53990880bbb3
Fernanda Andrade de Carvalho <fer.candrade21@gmail.com>
2022-10-06 19:03:06 +0100

    feat: cria componente input generico

---
+++ src/components/InputField.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+function InputField({
+  label, value, name, type, className, onChange,
+}) {
+  return (
+    <div>
+      <label htmlFor={ name }>{ label }</label>
+      <input
+        className={ className }
+        value={ value }
+        name={ name }
+        type={ type }
+        onChange={ onChange }
+      />
+    </div>
+  );
+}
+
+export default InputField;
