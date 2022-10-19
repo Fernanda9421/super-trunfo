d7f0e602af6d0e7ce56496e3cfdbe66fb801fe24
Fernanda Andrade de Carvalho <fer.candrade21@gmail.com>
2022-10-19 20:31:21 +0100

    feat: cria página inicial - apresentação

---
+++ src/components/Home.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Super from '../images/super_trunfo.png';
+
+function Home() {
+  return (
+    <div className="presentation">
+      <img className="image-logo" src={ Super } alt="Super trunfo logo" />
+      <nav>
+        <ul>
+          <li><a className="nav-link" href="#create">Criar Carta</a></li>
+          <li><a className="nav-link" href="#cards">Cartas</a></li>
+        </ul>
+      </nav>
+    </div>
+  );
+}
+
+export default Home;
--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import Card from '../components/Card';
 import Form from '../components/Form';
+import Home from '../components/Home';
+import RenderCards from '../components/RenderCards';
 
 function Main() {
   return (
     <div className="container-page-main">
-      <Form />
-      <Card />
+      <Home />
+
+      <section id="create">
+        <Form />
+        <Card />
+      </section>
+      <section id="cards">
+        <RenderCards />
+      </section>
     </div>
   );
 }
