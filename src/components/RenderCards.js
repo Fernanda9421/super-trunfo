a25ee1f1c68f68ab3be6d164c0f0092062c4e611
Fernanda Andrade de Carvalho <fer.candrade21@gmail.com>
2022-10-21 17:33:48 +0100

    feat: componente que renderiza todas as cartas

--- src/components/RenderCards.js
+++ src/components/RenderCards.js
@@ -1,9 +1,32 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import Context from '../context/Context';
+import Card from './Card';
 
 function RenderCards() {
+  const { cards } = useContext(Context);
+
   return (
-    <div>
-      <p>Cartas</p>
+    <div className="main-cards">
+      {
+        cards && cards
+          .map(({
+            name, description, attr1, attr2, attr3, image, rare, trunfo,
+          }, index) => (
+            <div className="main-cards-s" key={ name }>
+              <Card
+                heading={ `Carta${index + 1}` }
+                name={ name }
+                description={ description }
+                attr1={ attr1 }
+                attr2={ attr2 }
+                attr3={ attr3 }
+                image={ image }
+                rare={ rare }
+                trunfo={ trunfo }
+              />
+            </div>
+          ))
+      }
     </div>
   );
 }
